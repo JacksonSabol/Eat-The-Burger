670f70bde4d40e3529a523122e46bd30121e3ad0
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-04 11:09:05 -0800

    update connection to work with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Import MySQL Node Module
 var mysql = require("mysql");
 
-// Create a connection to localhost and the MySQL database
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+// Create a connection to JawsDB or localhost and the MySQL database
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burgers_db"
+	});
+};
 
 connection.connect(function (err) {
     // Check for errors
