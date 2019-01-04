db5bc18c406ca3a69ac25d3300277ed2af9f9f14
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-04 11:29:25 -0800

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
     devoured BOOLEAN default 0,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
